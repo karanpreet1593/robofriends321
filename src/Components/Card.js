c8a257642c3934a4940ef3bc6540f1af69995d99
Ranjeet Singh <PC@MacBook-Air.local>
2021-10-29 17:21:43 -0400

    optmized our by reducing the robo image size

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const Card = (props) => {
      return(
              <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-	             <img alt ='robots'src={`https://robohash.org/${props.id}?200x200`} />
+	             <img alt ='robots'src={`https://robohash.org/${props.id}?size=200x200`} />
 	             <div>
 		              <h2> {props.name} </h2>
 		              <p> {props.email} </p>
