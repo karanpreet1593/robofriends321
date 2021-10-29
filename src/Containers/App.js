7d4a5a34ae756a109dcdfe5f9468035bcb82bf0b
Ranjeet Singh <PC@MacBook-Air.local>
2021-10-29 16:06:38 -0400

    included redux in our app and also made in PWA

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import Cardlist from '../Components/Cardlist';
 import SearchBox from '../Components/SearchBox';
 import 'tachyons';
@@ -6,40 +7,47 @@ import './App.css';
 import Scroll from '../Components/Scroll';
 import ErrorBoundry from '../Components/ErrorBoundry';
 
+import {setSearchField, requestRobots} from '../action';
+// import { requestRobots } from '../reducers';
 
+
+const mapStateToProps = state => {
+	return{
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending:state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) =>{
+	return{
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	} 
+}
 
 class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots: [],
-			searchfield : ''
+	
+		componentDidMount(){
+			this.props.onRequestRobots();
+    	}
+	
 
-		}
-	}
-
-    componentDidMount(){
-    	fetch('https://jsonplaceholder.typicode.com/users')
-    	.then(response=> response.json())
-    	.then(users=>this.setState({robots: users}));
-    	
-    }
-
-	onSearchChange = (event)=>{
-		this.setState({searchfield: event.target.value})
-		
-	}
 	render(){
-		const filteredRobots = this.state.robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		const {searchField, onSearchChange, robots,isPending} = this.props
+
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
-		if (this.state.robots.length ===0){
-			return <h1 className='tc'  >Loading</h1>
-		} else {
-			return(
+		
+			return isPending ?
+			 <h1 className='tc'  >Loading</h1> :
+
+		(
 			<div className='tc'>
 			  <h1 className='f1' >RoboFriends</h1>
-			  < SearchBox searchChange={this.onSearchChange}/>
+			  < SearchBox searchChange={onSearchChange}/>
 			  <Scroll>
                <ErrorBoundry>
 			  	 <Cardlist robots={filteredRobots} />
@@ -49,6 +57,10 @@ class App extends Component {
 		);
 		}
 		
-	}
-}
-export default App;
+	}	
+
+	
+		
+	
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
